b80bb9b0b230228d471ac7b46a4fbb0819370631
mimithamg <mimithamg@gmail.com>
2022-12-24 19:19:32 +0530

    improved

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 let navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    shoppingCart.classList.remove('active');
-    loginForm.classList.remove('active');
-}
 
 window.onscroll = () =>{
     searchForm.classList.remove('active');
